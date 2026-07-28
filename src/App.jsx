ff1c1b0f84282d91475a729f7fee58736d9ddf64
Tiffany Pan <tiffanypantw@gmail.com>
2026-07-28 15:38:50 +0800

    L2S1 W2 套路獵人 上架

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,7 @@ import AdminUploadW11 from './pages/admin/AdminUploadW11';
 import AdminUploadL1S1W1 from './pages/admin/AdminUploadL1S1W1';
 import AdminUploadL1S1W2 from './pages/admin/AdminUploadL1S1W2';
 import AdminUploadL2S1W1 from './pages/admin/AdminUploadL2S1W1';
+import AdminUploadL2S1W2 from './pages/admin/AdminUploadL2S1W2';
 import AdminUploadL3S1W1 from './pages/admin/AdminUploadL3S1W1';
 import WeekLearning from './pages/WeekLearning';
 import Profile from './pages/Profile';
@@ -108,6 +109,7 @@ function App() {
               <Route path="upload-l1s1-w1" element={<AdminUploadL1S1W1 />} />
               <Route path="upload-l1s1-w2" element={<AdminUploadL1S1W2 />} />
               <Route path="upload-l2s1-w1" element={<AdminUploadL2S1W1 />} />
+              <Route path="upload-l2s1-w2" element={<AdminUploadL2S1W2 />} />
               <Route path="upload-l3s1-w1" element={<AdminUploadL3S1W1 />} />
             </Route>
 
